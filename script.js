c736fd9537ad3340ba689da72dee2c3110474d51
Nahidjc <nahidjc80@gmail.com>
2021-09-02 00:24:47 +0600

    all books show

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ const searchFunction = (searchValue) => {
 // book details
 const showBook = books => {
     let allBooksDetails = document.getElementById('all-books');
-    let singleBook = document.getElementById('single-book');
+    allBooksDetails.textContent = '';
+
     books.forEach((book) => {
         console.log(book);
         let imageId = book.cover_i;
@@ -30,9 +31,14 @@ const showBook = books => {
         // url = "https://openlibrary.org/authors/OL62134A.json"
         let imageUrl = ` https://covers.openlibrary.org/b/id/${imageId}-M.jpg`
 
+        // Create col element for showing each booksDetails
+        let singleBook = document.createElement('div');
+        singleBook.classList.add('col-md-4');
+
+
         singleBook.innerHTML = `
             <div class="card" style="width: 18rem;">
-            <img src="${imageUrl}" class="card-img-top" alt="...">
+            <img src="${imageUrl}" class="card-img-top" height="300">
             <div class="card-body">
                 <h5>Title: ${book.title}</h5>
                 <p>Author: ${book.author_name}</p>
