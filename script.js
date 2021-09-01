22d13bd4b87ba19428eb31d3bf20c93afa174ea2
Nahidjc <nahidjc80@gmail.com>
2021-09-01 23:15:37 +0600

    find url from search text

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Paichi")
+document.getElementById('search-btn').addEventListener('click', function () {
+    let searchText = document.getElementById('search-text');
+    let searchValue = searchText.value;
+    console.log(searchValue);
+    url = `http://openlibrary.org/search.json?q=${searchValue}`;
+    console.log(url);
+})
