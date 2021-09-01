c16722b85cca05806832b203985708d3741c52c8
Nahidjc <nahidjc80@gmail.com>
2021-09-02 00:35:39 +0600

    total count search book

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ const searchFunction = (searchValue) => {
     fetch(url)
         .then(response => response.json())
         .then(data => {
+            console.log(data);
+            let totalBooks = data.numFound;
+            document.getElementById('total-books').innerText = `
+            Total Search Book : ${totalBooks}
+            `
             showBook(data.docs);
         }).catch((error) => {
             console.error('Error:', error);
@@ -25,22 +30,21 @@ const showBook = books => {
     allBooksDetails.textContent = '';
 
     books.forEach((book) => {
-        console.log(book);
+
         let imageId = book.cover_i;
-        // authorKey = book.author_name
-        // url = "https://openlibrary.org/authors/OL62134A.json"
         let imageUrl = ` https://covers.openlibrary.org/b/id/${imageId}-M.jpg`
 
         // Create col element for showing each booksDetails
         let singleBook = document.createElement('div');
-        singleBook.classList.add('col-md-4');
+        singleBook.classList.add('col-md-3');
+        singleBook.classList.add('mb-3');
 
 
         singleBook.innerHTML = `
             <div class="card" style="width: 18rem;">
             <img src="${imageUrl}" class="card-img-top" height="300">
             <div class="card-body">
-                <h5>Title: ${book.title}</h5>
+                <h6>Book Name: ${book.title}</h6>
                 <p>Author: ${book.author_name}</p>
                 <small>Publisher: ${book.publisher}</small> <br>
                 <small>First Publish: ${book.first_publish_year}</small> <br>
