c03d9296792027bcbedd918998214aebae717091
Nahidjc <nahidjc80@gmail.com>
2021-09-01 23:31:02 +0600

    single book find

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ document.getElementById('search-btn').addEventListener('click', function () {
     let searchText = document.getElementById('search-text');
     let searchValue = searchText.value;
     console.log(searchValue);
+    searchFunction(searchValue);
+
+})
+
+
+// find books details  search by book name 
+const searchFunction = (searchValue) => {
     url = `http://openlibrary.org/search.json?q=${searchValue}`;
     console.log(url);
-})
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            showBook(data.docs);
+        }).catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+// book details
+const showBook = books => {
+
+    books.forEach((book) => {
+        console.log(book.title);
+
+    })
+}
